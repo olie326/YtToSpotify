dbca9d5fc8e5a4ba202dd9640ede1c5ada051fc5
Oliver Lee <ol558@nyu.edu>
2024-01-28 10:50:06 -0500

    remove creds

--- yt_scraper/test.py
+++ yt_scraper/test.py
@@ -1,24 +1,13 @@
 
-
-myDict = {
-    'title': 'this is my title',
-    'song1': {
-        'song_title': 'something1',
-        'artist_name': 'name1'
-    },
-    'song2': {
-        'song_title': 'something2',
-        'artist_name': 'name2'
-    },
-}
-
-def function(dictionary):
+def function(nums):
+    myList = []
+    for i in nums:
+        new_nums = nums
+        new_nums.remove(i)
+        myList.append(sum(new_nums))
     
-    for index, items in enumerate(dictionary):
-        if items == 'title':    
-            continue
-        print(index, dictionary[items]['song_title'])
+    return myList
 
+array = [1, 2, 3, 4]
 
-function(myDict)
-    
+function(array)
